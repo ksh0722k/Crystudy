d7e70a92d0c1dc2013c9484f5bf7544b08272b8e
JaneseoHyeon <49301073+JaneseoHyeon@users.noreply.github.com>
2021-05-09 17:08:05 +0900

    main 틀/ css전

--- crystudy/src/pages/Main.js
+++ crystudy/src/pages/Main.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import { Button } from "@material-ui/core";
+import "../scss/pages/_Main.scss";
 
 function Main() {
+    const userName = "홍길동";
+
     return (
-        <div>
-            <h1>크리스터디 메인</h1>
+        <div className="main-cont">
+            <div className="main-title">환영합니다,<br/>{userName} 님!</div>
+            <div className="main-center-cont">
+                <Button className="main-center-Rcont"><SearchIcon/></Button>
+                <Button className="main-center-Lcont"><DescriptionOutlinedIcon/></Button>
+            </div>
         </div>
     )
 }
